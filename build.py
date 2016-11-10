0cd8bc8f5598b4b67210ac1e15b0913ce4a8aece
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-11-10 15:22:40 +0100

    first step for running integration tests in teamcity

--- build.py
+++ build.py
@@ -6,11 +6,12 @@ use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+use_plugin("python.integrationtest")
 
 
 name = "spotnik"
 default_task = "publish"
-
+version = "0.1"
 
 @init
 def set_properties(project):
@@ -18,3 +19,11 @@ def set_properties(project):
 
     project.depends_on('boto3')
     project.build_depends_on('unittest2')
+
+@init(environments='teamcity')
+def set_properties_for_teamcity_builds(project):
+    import os
+    project.set_property('teamcity_output', True)
+    project.version = '%s-%s' % (project.version, os.environ.get('BUILD_NUMBER', 0))
+    project.default_task = ['clean', 'install_build_dependencies', 'publish']
+    project.set_property('install_dependencies_index_url', os.environ.get('PYPIPROXY_URL'))
