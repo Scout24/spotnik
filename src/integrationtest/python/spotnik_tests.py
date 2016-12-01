37b9e650fab3d1ca33f0706de9a8cc90bfaf6619
mlefringhausen <martin.lefringhausen@immobilienscout24.de>
2016-12-01 14:27:10 +0100

    do not wait longer then needed for service available

--- src/integrationtest/python/spotnik_tests.py
+++ src/integrationtest/python/spotnik_tests.py
@@ -10,6 +10,7 @@ import unittest2
 from spotnik.spotnik import main, EC2, AUTOSCALING, ReplacementPolicy
 from subprocess import check_call, call
 
+
 class SpotnikTests(unittest2.TestCase):
     def test_spotnik_main(self):
         self.create_application_stack()
@@ -20,7 +21,7 @@ class SpotnikTests(unittest2.TestCase):
 
         # second run of spotnik should attach the running spot instance to the asg
         self.assert_spotnik_request_instances(0)
-        elb_dns_name, asg_name = self.get_cf_output()
+        _, asg_name = self.get_cf_output()
         asg = AUTOSCALING.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name])['AutoScalingGroups'][0]
         on_demand_instances, spot_instances = ReplacementPolicy(asg).get_instances()
         self.assertEqual(len(on_demand_instances), 1)
@@ -44,29 +45,41 @@ class SpotnikTests(unittest2.TestCase):
         self.assert_service_is_available()
 
     def assert_service_is_available(self):
-        elb_dns_name, asg_name = self.get_cf_output()
-        port = 22
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((elb_dns_name, port))
-        s.sendall('Hello, world')
-        data = s.recv(1024)
-        print(data)
-        s.close()
-        self.assertIn('SSH', data.upper())
+        self.assertTrue(self.is_service_available())
 
-    def get_num_spot_requests(self):
+    @staticmethod
+    def get_num_spot_requests():
         response = EC2.describe_spot_instance_requests()
         return len(response['SpotInstanceRequests'])
+
+    def is_service_available(self):
+        try:
+            elb_dns_name, asg_name = self.get_cf_output()
+            port = 22
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((elb_dns_name, port))
+            s.sendall('Hello, world')
+            data = s.recv(1024)
+            print(data)
+            s.close()
+        except Exception:
+            return False
+        return 'SSH' in data.upper()
 
     def create_application_stack(self):
         call("cf delete --confirm src/integrationtest/integrationtest_stacks.yaml", shell=True)
         check_call("cf sync --confirm src/integrationtest/integrationtest_stacks.yaml", shell=True)
-        time.sleep(120)
+        counter = 0
+        while counter < 300 and not self.is_service_available():
+            counter += 1
+            time.sleep(1)
+        self.assert_service_is_available()
 
     def delete_application_stack(self):
         check_call("cf delete --confirm src/integrationtest/integrationtest_stacks.yaml", shell=True)
 
-    def get_cf_output(self):
+    @staticmethod
+    def get_cf_output():
         client = boto3.client('cloudformation', region_name="eu-west-1")
         response = client.describe_stacks(StackName='SimpleElbAppSpotnikIntegrationtest')
         outputs = response['Stacks'][0]['Outputs']
