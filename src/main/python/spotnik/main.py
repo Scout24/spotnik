5c744428255ccba771d13bfcf80267dbeb8b3d40
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-12-09 15:19:46 +0100

    Also fail if an ASG-thread fails

--- src/main/python/spotnik/main.py
+++ src/main/python/spotnik/main.py
@@ -49,9 +49,14 @@ def run_regional_thread(region_name):
         spotnik_asgs = Spotnik.get_spotnik_asgs(region_name)
         logger.info("Found %d spotnik ASGs", len(spotnik_asgs))
 
+        asg_threads = []
         for asg in spotnik_asgs:
             asg_thread = threading.Thread(target=run_asg_thread, args=(region_name, asg))
             asg_thread.start()
+            asg_threads.append(asg_thread)
+
+        for asg_thread in asg_threads:
+            asg_thread.join()
     except Exception:
         logger.exception("Thread failed:")
         global _ERROR_IN_MAIN
@@ -60,20 +65,25 @@ def run_regional_thread(region_name):
 
 def run_asg_thread(region_name, asg):
     logger = logging.getLogger("spotnik.%s.%s" % (region_name, asg['AutoScalingGroupName']))
-    spotnik = Spotnik(region_name, asg, logger=logger)
+    try:
+        spotnik = Spotnik(region_name, asg, logger=logger)
 
-    logger.info("Processing ASG with this config: \n%s", pformat(asg))
-    spot_request, spot_instance_id = spotnik.get_pending_spot_resources()
-    if spot_instance_id:
-        logger.info("Instance %r is ready to be attached to ASG", spot_instance_id)
-        spotnik.attach_spot_instance(spot_instance_id, spot_request)
-        spotnik.untag_spot_request(spot_request)
-    elif spot_request:
-        logger.info("ASG has pending spot request %r.", spot_request['SpotInstanceRequestId'])
-        # Amazon processing our request, but no instance yet
-        return
-    else:
-        spotnik.make_spot_request()
+        logger.info("Processing ASG with this config: \n%s", pformat(asg))
+        spot_request, spot_instance_id = spotnik.get_pending_spot_resources()
+        if spot_instance_id:
+            logger.info("Instance %r is ready to be attached to ASG", spot_instance_id)
+            spotnik.attach_spot_instance(spot_instance_id, spot_request)
+            spotnik.untag_spot_request(spot_request)
+        elif spot_request:
+            logger.info("ASG has pending spot request %r.", spot_request['SpotInstanceRequestId'])
+            # Amazon processing our request, but no instance yet
+            return
+        else:
+            spotnik.make_spot_request()
+    except Exception:
+        logger.exception("Thread failed:")
+        global _ERROR_IN_MAIN
+        _ERROR_IN_MAIN = True
 
 
 if __name__ == "__main__":
--- src/unittest/python/main_tests.py
+++ src/unittest/python/main_tests.py
@@ -1,10 +1,19 @@
 from __future__ import print_function, absolute_import, division
 
+import time
 import unittest2
 
 from mock import Mock, patch
 
 from spotnik.main import main
+
+
+def fail_eventually(*args, **kwargs):
+    # Test that the thread is really join()ed and it's not just
+    # a race condition that makes the test pass.
+    time.sleep(.1)
+    raise Exception
+
 
 class MainTests(unittest2.TestCase):
     @patch("spotnik.main.get_aws_region_names")
@@ -14,5 +23,13 @@ class MainTests(unittest2.TestCase):
         #   - make sure it gets notified about errors
         #   - propagates the error to its caller
         mock_get_aws_region_names.return_value = ['region_one', 'region_two']
-        mock_spotnik.get_spotnik_asgs.side_effect = Exception
+        mock_spotnik.get_spotnik_asgs.side_effect = fail_eventually
+        self.assertRaises(Exception, main)
+
+    @patch("spotnik.main.get_aws_region_names")
+    @patch("spotnik.main.Spotnik")
+    def test_main_fails_if_asg_thread_fails(self, mock_spotnik, mock_get_aws_region_names):
+        mock_get_aws_region_names.return_value = ['region_one', 'region_two']
+        mock_spotnik.get_spotnik_asgs.return_value = [{'AutoScalingGroupName': 'foo'}]
+        mock_spotnik.side_effect = fail_eventually
         self.assertRaises(Exception, main)
